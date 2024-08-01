ad4d9999b73fb08a2a1b6a7fde4798c6456ae2ff
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-01 15:22:09 +0900

    add 光の設定

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmatsuba <rmatsuba@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 00:30:07 by rmatsuba          #+#    #+#             */
-/*   Updated: 2024/07/31 12:03:00 by rmatsuba         ###   ########.fr       */
+/*   Updated: 2024/08/01 15:20:33 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,20 @@ void	my_mlx_pixel_put(t_rt *rt, int x, int y, int color)
 	dst = rt->addr + (y * rt->line_len + x * (rt->bpp / 8));
 	*(unsigned int *)dst = color;
 }
+t_vec3 vec3_scale(t_vec3 v, double s) {
+    return vec3_init(v.x * s, v.y * s, v.z * s);
+}
 
-
+t_vec3 vec3_reflect(t_vec3 v, t_vec3 n) {
+    double dot = vec3_dot(v, n);
+    return vec3_sub(vec3_scale(n, 2 * dot), v);
+}
 void	print_sphere(t_rt *rt)
 {
 	t_scene	*scene = rt->scene;
+    double diffuse_strength = 0.8;
+    double specular_strength = 0.5;
+     int shininess = 32;
 	for (double y = 0;  y < rt->height; ++y)
 	{
 		for (double x = 0; x < rt->width; ++x)
@@ -43,7 +52,26 @@ void	print_sphere(t_rt *rt)
 			double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - scene->sphere->diameter * scene->sphere->diameter;
 			double d = b * b - 4 * a * c;
 			if (d >= 0)
-				my_mlx_pixel_put(rt, x, y, 0xFFFFFF);
+            {
+                double t = (-b + sqrt(d)) / (2.0 * a);
+                t_vec3 intersection = vec3_add(*scene->camera->view_point, vec3_scale(dir_vec, t));
+                t_vec3 normal = vec3_norm(vec3_sub(intersection, *scene->sphere->center));
+                t_vec3 light_vec = vec3_norm(vec3_sub(*scene->light->light_point, intersection));
+                t_vec3 view_vec = vec3_norm(vec3_sub(*scene->camera->view_point, intersection));
+                t_vec3 reflect_vec = vec3_reflect(vec3_scale(light_vec, -1), normal);
+
+                double diff = fmax(vec3_dot(normal, light_vec), 0.0) * diffuse_strength;
+
+                double spec = pow(fmax(vec3_dot(view_vec, reflect_vec), 0.0), shininess) * specular_strength;
+
+                double brightness = scene->ambi_light->ratio + diff + spec;
+                int r = (int)(fmin(scene->sphere->rgb->r * brightness , 1.0) * 255);
+                int g = (int)(fmin(scene->sphere->rgb->g * brightness , 1.0) * 255);
+                int b = (int)(fmin(scene->sphere->rgb->b * brightness , 1.0) * 255);
+
+                int color = (r << 16) | (g << 8) | b;
+                my_mlx_pixel_put(rt, x, y, color);
+            }
 			else
 				my_mlx_pixel_put(rt, x, y, 0x00000000);
 		}
@@ -81,8 +109,8 @@ int	main(int argc, char **argv)
 	scene = init_scene();
 	scene = parse_file(argv[1], scene);
 	rt->scene = scene;
-	/* print_sphere(rt); */
-	print_plane(rt);
+	 print_sphere(rt); 
+	// print_plane(rt);
 	mlx_put_image_to_window(rt->mlx, rt->mlx_win, rt->img, 0, 0);
 	hook_event(rt);
 	mlx_loop(rt->mlx);
