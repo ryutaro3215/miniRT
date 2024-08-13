676b43bd20b4cdd99e180f886b996d4ffb3f3a75
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-13 11:19:12 +0900

    calc_distance関数の作成

--- ray_cross/seach_nearest_obj.c
+++ ray_cross/seach_nearest_obj.c
@@ -6,19 +6,44 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 23:08:42 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/08/12 23:12:02 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/08/13 11:18:41 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
+static double calc_distance(t_object *obj, double x, double y)
+{
+    if(obj->type == SPHERE)
+        return (calc_sp_distance(obj, x, y));
+    else if(obj->type == PLANE)
+        return(calc_pl_distance(obj, x, y));
+    else if(obj->type == CYLINDER)
+        return(calc_cy_distance(obj, x, y));
+}
+
 t_object seach_nearest_obj(t_rt *rt, double x, double y)
 {
-    t_object    nearest_obj;
+    t_object    *nearest_obj;
     t_object    *obj;
     t_object    *tmp;
-    double      t;
-    double      min_t;
+    double      distance;
+    double      min_distance;
     
-    min_t = 1000000000;
+    // TODO defineで定義するようにする
+    min_distance = 1000000000;
+
+    nearest_obj = NULL;
+    obj = rt->scene->object;
+    while(obj)
+    {
+        distance = 0;
+        distance = calc_distance(obj, x, y);
+        if(distance < min_distance)
+        {
+            min_distance = distance;
+            nearest_obj = obj;
+        }
+        obj = obj->next;
+    }
 }
