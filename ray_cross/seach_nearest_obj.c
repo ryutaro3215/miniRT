6afc904f721016ddc58fd3f1facf76b25cf29d23
ryutaro3215 <ryutaro140515@icloud.com>
2024-08-14 18:59:39 +0900

    make calc_cy_distance

--- ray_cross/seach_nearest_obj.c
+++ ray_cross/seach_nearest_obj.c
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 23:08:42 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/08/13 20:09:34 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/08/14 18:00:50 by rmatsuba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,28 +45,81 @@ double calc_pl_distance(t_object *object, t_vec3 screen_vec, t_camera *camera)
 	return (molecule / denominator);
 }
 
-double calc_cy_distance(t_object *object, t_vec3 screen_vec, t_camera *camera)
+t_vec3  *calc_cy_intersections(t_object *obj, t_vec3 dir, t_vec3 cam2cy, t_camera *camera)
 {
-    	double	a;
+    double	a;
 	double	b;
 	double	c;
 	double	d;
 	t_vec3	*intersections;
-    t_vec3    dir;
-    t_vec3    cam2cyl;
+
+	a = vec3_dot(dir, dir) - pow(vec3_dot(dir, *obj->axic_vec), 2);
+	b = 2 * (vec3_dot(dir, cam2cy) - vec3_dot(dir, *obj->axic_vec)
+			* vec3_dot(*obj->axic_vec, cam2cy));
+	c = vec3_dot(cam2cy, cam2cy) - pow(vec3_dot(*obj->axic_vec, cam2cy), 2)
+		- obj->diameter / 2 * obj->diameter / 2;
+	d = b * b - 4 * a * c;
+	if (d < 0)
+		return (NULL);
+	intersections = (t_vec3 *)malloc(sizeof(t_vec3) * 2);
+	if (intersections == NULL)
+		return (NULL);
+	intersections[0] = vec3_add(*camera->view_point, 
+			vec3_mul(dir, ((-b - sqrt(d)) / (2 * a))));
+	intersections[1] = vec3_add(*camera->view_point,
+			vec3_mul(dir, ((-b + sqrt(d)) / (2 * a))));
+	return (intersections);
+}
+
+bool    calc_cy_height(t_object *object, t_vec3 *intersections, t_camera *camera)
+{
+    bool	is_intersect_p1;
+	bool	is_intersect_p2;
+	t_vec3	axic;
+	t_vec3	cyl_top;
+	t_vec3	cyl_bottom;
+
+	cyl_top = vec3_add(*camera->view_point, 
+			vec3_mul(*object->axic_vec, object->height / 2));
+	cyl_bottom = vec3_sub(*camera->view_point, 
+			vec3_mul(*object->axic_vec, object->height / 2));
+	axic = *object->axic_vec;
+	is_intersect_p1 = vec3_dot(vec3_sub(intersections[0], cyl_bottom), axic) >= 0 &&
+		vec3_dot(vec3_sub(intersections[0], cyl_top), axic) <= 0;
+	is_intersect_p2 = vec3_dot(vec3_sub(intersections[1], cyl_bottom), axic) >= 0 &&
+		vec3_dot(vec3_sub(intersections[1], cyl_top), axic) <= 0;
+	if ( is_intersect_p1|| is_intersect_p2)
+		return (true);
+	else
+		return (false);
+}
+
+double calc_cy_distance(t_object *object, t_vec3 screen_vec, t_camera *camera)
+{
+    t_vec3  dir;
+    t_vec3  cam2cy;
+    t_vec3  *intersections;
+    bool    flag;
+    double  distance;
 
     dir = vec3_norm(vec3_sub(screen_vec, *camera->view_point));
-    cam2cyl = vec3_sub(*camera->view_point, *object->center);
-	a = vec3_dot(dir, dir) - pow(vec3_dot(dir, *object->axic_vec), 2);
-	b = 2 * (vec3_dot(dir, cam2cyl) - vec3_dot(dir, *object->axic_vec)
-			* vec3_dot(*object->axic_vec, cam2cyl));
-	c = vec3_dot(cam2cyl, cam2cyl) - pow(vec3_dot(*object->axic_vec, cam2cyl), 2)
-		- object->diameter / 2 * object->diameter / 2;
-	d = b * b - 4 * a * c;
-    double outer = (-b - sqrt(d)) / (2 * a);
-    double inner = (-b + sqrt(d)) / (2 * a);
-    // TODO まだ未完成・・・
+    cam2cy = vec3_sub(*camera->view_point, *object->center);
+    intersections = calc_cy_intersections(object, dir, cam2cy, camera);
+    if (intersections == NULL)
+        return (-1);
+    flag = calc_cy_height(object, intersections, camera);
+    if (flag == false)
+	{
+		free(intersections);
+        return (-1);
+	}
+	distance = vec3_mag(vec3_sub(*camera->view_point, intersections[0]));
+	if (distance > vec3_mag(vec3_sub(*camera->view_point, intersections[1])))
+		distance = vec3_mag(vec3_sub(*camera->view_point, intersections[1]));
+    free(intersections);
+	return (distance);
 }
+
 double calc_distance(t_object *obj, t_vec3 screen_vec, t_camera *camera)
 {
     if(obj->type == SPHERE)
