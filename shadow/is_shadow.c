542b4b33b359d6cc9adf14c4ffff4cdca5900552
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-16 18:54:32 +0900

    init

--- shadow/is_shadow.c
+++ shadow/is_shadow.c
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 12:36:45 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/08/13 20:01:17 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/08/16 18:54:20 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,6 @@ bool is_shadow(t_scene *scene ,double t, t_vec3 dir_vec)
     t_vec3 intersec2light = vec3_sub(*scene->light->light_point, p);
     t_vec3 l = vec3_norm(intersec2light);
     t_vec3 start = vec3_add(p, vec3_mul(l, 0.0001));
+    double light_dist = vec3_mag(intersec2light) - 0.0001;
+    // シャドウベクトルが好転とぶつかり、かつ、それが最も近いオブジェクトにぶつかるかどうか
 }
