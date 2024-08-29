5f47ac3b312edce0c20a3055b43810d8805acd88
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-29 22:09:57 +0900

    discriminant関数の修正

--- ray_cross/ray_cross.c
+++ ray_cross/ray_cross.c
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 18:23:33 by rmatsuba          #+#    #+#             */
-/*   Updated: 2024/08/26 23:54:07 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/08/29 14:59:35 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,31 +152,29 @@ bool	discriminant_cylinder(t_rt *rt, t_vec3 screen_vec, t_object *object)
 	intersections = get_intersections(rt, dir, cam2cyl, object);
 	if (intersections == NULL)
 		return (false);
-	flag = is_height_range(rt, intersections,object);
+	// flag = is_height_range(rt, intersections,object);
      t_vec3 center2inner = vec3_sub(intersections[1],*object->center);
     t_vec3 center2outer = vec3_sub(intersections[0],*object->center);
     
     double h_outer = vec3_dot(center2outer, *object->axic_vec);
     double h_inner = vec3_dot(center2inner, *object->axic_vec);
-   if (h_inner >= 0 && h_inner <= object->height)
+   if  ( h_outer>=0 && h_outer <= object->height)
     {
-        t_vec3 projection_on_axis_inner = vec3_mul(*object->axic_vec, h_inner);
-        *object->normal_vec = vec3_norm(vec3_sub(projection_on_axis_inner,center2inner));
+        flag = true;
+        // t_vec3 projection_on_axis_inner = vec3_mul(*object->axic_vec, h_inner);
+        // *object->normal_vec = vec3_norm(vec3_sub(projection_on_axis_inner,center2inner));
  
     }
-else if (h_outer >= 0 && h_outer <= object->height)
+else if ( h_inner>=0 &&  h_inner <= object->height)
 {
-    t_vec3 projection_on_axis_outer = vec3_mul(*object->axic_vec, h_outer);
-    *object->normal_vec = vec3_norm(vec3_sub(center2outer, projection_on_axis_outer));
-
+    flag = true;
+    // t_vec3 projection_on_axis_outer = vec3_mul(*object->axic_vec, h_outer);
+    // *object->normal_vec = vec3_norm(vec3_sub(center2outer, projection_on_axis_outer));
 }
-
-
+else
+    flag = false;
 	free(intersections);
-	if (flag == true)
-		return (true);
-	else
-		return (false);
+    return (flag);
 }
 
 void	draw_cylinder(t_rt *rt,double x, double y, t_object *nearest_obj)
@@ -186,7 +184,9 @@ void	draw_cylinder(t_rt *rt,double x, double y, t_object *nearest_obj)
 
     screen_vec = vec3_init(2 * x / rt->width - 1.0, 2 * y / rt->height - 1.0, 0);
     is_drawable = discriminant_cylinder(rt, screen_vec,nearest_obj);
+    // is_drawable = calc_cy_distance(nearest_obj, screen_vec, rt->scene->camera->view_point);
     // printf("%f\n",nearest_obj->normal_vec->x);
+    // if(is_drawable >= 0)
     if (is_drawable == true)
         my_mlx_pixel_put(rt, x, y, phong_calc(rt->scene, screen_vec,nearest_obj,rt));
         // my_mlx_pixel_put(rt, x, y, int_to_hex_color(nearest_obj->rgb));
--- ray_cross/seach_nearest_obj.c
+++ ray_cross/seach_nearest_obj.c
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 23:08:42 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/08/27 19:51:51 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/08/29 12:17:39 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,39 +51,7 @@ double calc_pl_distance(t_object *object, t_vec3 dir, t_vec3 *source_point)
 
 
 
-t_vec3  *calc_cy_intersections(t_object *obj, t_vec3 dir, t_vec3 cam2cy, t_vec3 *source_point)
-{
-    double	a;
-	double	b;
-	double	c;
-	double	d;
-	t_vec3	*intersections;
 
-	a = vec3_dot(dir, dir) - pow(vec3_dot(dir, *obj->axic_vec), 2);
-	b = 2 * (vec3_dot(dir, cam2cy) - vec3_dot(dir, *obj->axic_vec)
-			* vec3_dot(*obj->axic_vec, cam2cy));
-
-	c = vec3_dot(cam2cy, cam2cy) - pow(vec3_dot(*obj->axic_vec, cam2cy), 2)
-		- obj->diameter / 2 * obj->diameter / 2;
-    // a = vec3_mag(vec3_cross(dir, *obj->axic_vec));
-    // a*=a;
-    //  b = 2 * vec3_dot(vec3_cross(dir, *obj->axic_vec), vec3_cross(vec3_sub(*source_point, *obj->center), *obj->axic_vec));
-    //  c = vec3_mag(vec3_cross(vec3_sub(*source_point, *obj->center),  *obj->axic_vec));
-	// c = c * c - (obj->diameter/2) *(obj->diameter/2);
-    if(cam2cy.x == 0 && cam2cy.y == 0 && cam2cy.z == 0)
-        ;
-	d = b * b - 4 * a * c;
-	if (d < 0)
-		return (NULL);
-	intersections = (t_vec3 *)malloc(sizeof(t_vec3) * 2);
-	if (intersections == NULL)
-		return (NULL);
-	intersections[0] = vec3_add(*source_point, 
-			vec3_mul(dir, ((-b - sqrt(d)) / (2 * a))));
-	intersections[1] = vec3_add(*source_point,
-			vec3_mul(dir, ((-b + sqrt(d)) / (2 * a))));
-	return (intersections);
-}
 
 bool    calc_cy_height(t_object *object, t_vec3 *intersections, t_vec3 *source_point)
 {
@@ -129,7 +97,39 @@ double  calc_cy_t2(t_object *obj, t_vec3 dir, t_vec3 cam2cy, int flag,t_vec3 *so
     
     return (-b - sqrt(d)) / (2 * a);
 }
+t_vec3  *calc_cy_intersections(t_object *obj, t_vec3 dir, t_vec3 cam2cy, t_vec3 *source_point)
+{
+    double	a;
+	double	b;
+	double	c;
+	double	d;
+	t_vec3	*intersections;
 
+	// a = vec3_dot(dir, dir) - pow(vec3_dot(dir, *obj->axic_vec), 2);
+	// b = 2 * (vec3_dot(dir, cam2cy) - vec3_dot(dir, *obj->axic_vec)
+	// 		* vec3_dot(*obj->axic_vec, cam2cy));
+
+	// c = vec3_dot(cam2cy, cam2cy) - pow(vec3_dot(*obj->axic_vec, cam2cy), 2)
+	// 	- obj->diameter / 2 * obj->diameter / 2;
+    a = vec3_mag(vec3_cross(dir, *obj->axic_vec));
+    a*=a;
+     b = 2 * vec3_dot(vec3_cross(dir, *obj->axic_vec), vec3_cross(vec3_sub(*source_point, *obj->center), *obj->axic_vec));
+     c = vec3_mag(vec3_cross(vec3_sub(*source_point, *obj->center),  *obj->axic_vec));
+	c = c * c - (obj->diameter/2) *(obj->diameter/2);
+    if(cam2cy.x == 0 && cam2cy.y == 0 && cam2cy.z == 0)
+        ;
+	d = b * b - 4 * a * c;
+	if (d < 0)
+		return (NULL);
+	intersections = (t_vec3 *)malloc(sizeof(t_vec3) * 2);
+	if (intersections == NULL)
+		return (NULL);
+	intersections[0] = vec3_add(*source_point, 
+			vec3_mul(dir, ((-b - sqrt(d)) / (2 * a))));
+	intersections[1] = vec3_add(*source_point,
+			vec3_mul(dir, ((-b + sqrt(d)) / (2 * a))));
+	return (intersections);
+}
 double calc_cy_distance(t_object *object, t_vec3 dir, t_vec3 *source_point)
 {
 
@@ -154,13 +154,13 @@ double calc_cy_distance(t_object *object, t_vec3 dir, t_vec3 *source_point)
     double h_outer = vec3_dot(center2outer, *object->axic_vec);
     double h_inner = vec3_dot(center2inner, *object->axic_vec);
     printf("h_outer:%f, h_inner:%f\n", h_outer, h_inner);
-	distance = vec3_mag(vec3_sub(*source_point, intersections[0]));
-	if (h_inner>= 0 && h_inner <= object->height)
-        	distance = vec3_mag(vec3_sub(*source_point, intersections[1]));
-		// distance = calc_cy_t2(object, dir, vec3_sub(*source_point, *object->center), 1,source_point);
-    else if(h_outer >= 0 && h_outer <= object->height)
-        	distance = vec3_mag(vec3_sub(*source_point, intersections[0]));
-        // distance = calc_cy_t2(object, dir, vec3_sub(*source_point, *object->center), 0,source_point);
+	// distance = vec3_mag(vec3_sub(*source_point, intersections[0]));
+	if (h_outer >= 0 && h_outer <= object->height)
+        distance = calc_cy_t2(object, dir, vec3_sub(*source_point, *object->center), 0,source_point);
+        	// distance = vec3_mag(vec3_sub(*source_point, intersections[0]));
+    else if(h_inner>= 0 && h_inner <= object->height)
+		distance = calc_cy_t2(object, dir, vec3_sub(*source_point, *object->center), 1,source_point);
+        	// distance = vec3_mag(vec3_sub(*source_point, intersections[1]));
     else
         distance = -1;
     free(intersections);
