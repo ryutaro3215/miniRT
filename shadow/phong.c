bf18e04c7461917eeabdd008c819cafdc955ca99
yoshiddddd <yoshidakerapo@gmail.com>
2024-10-05 15:13:09 +0900

    push main

--- shadow/phong.c
+++ shadow/phong.c
@@ -6,7 +6,7 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 23:32:07 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/09/30 21:51:24 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/10/02 21:42:48 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 t_rgb check_color_is_valid(t_rgb a)
 {
-    // printf("a.r:%u,a.g:%u,a.b:%u\n",a.r,a.g,a.b);
     if(a.r > 1.0)
         a.r = 1.0;
     if(a.r < 0.0)
@@ -30,6 +29,22 @@ t_rgb check_color_is_valid(t_rgb a)
         a.b = 0.0;
     return a;
 }
+// t_rgb check_color_is_valid(t_rgb a)
+// {
+//     if(a.r > 255)
+//         a.r = 255;
+//     if(a.r < 0.0)
+//         a.r = 0.0;
+//     if(a.g > 255)
+//         a.g = 255;
+//     if(a.g < 0.0)
+//         a.g = 0.0;
+//     if(a.b > 255)
+//         a.b = 255;
+//     if(a.b < 0.0)
+//         a.b = 0.0;
+//     return a;
+// }
 t_rgb   color_init2(double r, double g, double b)
 {
     t_rgb   color;
@@ -38,6 +53,7 @@ t_rgb   color_init2(double r, double g, double b)
     color.g = g;
     color.b = b;
     return (check_color_is_valid(color));
+    // return color;
 }
 
 t_rgb color_mul(t_rgb a, t_rgb b)
@@ -46,6 +62,7 @@ t_rgb color_mul(t_rgb a, t_rgb b)
     a.g *= b.g;
     a.b *= b.b;
     return check_color_is_valid(a);
+    // return a;
 }
 
 t_rgb color_mul_scalar(t_rgb a, double b)
@@ -55,17 +72,19 @@ t_rgb color_mul_scalar(t_rgb a, double b)
     a.b *= b;
     return check_color_is_valid(a);
 }
+
 t_rgb color_add(t_rgb a, t_rgb b)
 {
     a.r += b.r;
     a.g += b.g;
     a.b += b.b;
     return check_color_is_valid(a);
+    // return a;
 }
 t_rgb nearest_objs_color(t_object *nearest_obj)
 {
-    t_rgb color;    
-    // int colorhex = nearest_obj->rgb->r | nearest_obj->rgb->b | nearest_obj->rgb->b ;
+    t_rgb color;
+    // int colorhex = nearest_obj->rgb->r | nearest_obj->rgb->b | nearest_obj->rgb->b;
     // double red = (double)(colorhex >> 16 & 0xff) / 255;
 	// double green = (double)(colorhex >> 8 & 0xff) / 255;
 	// double blue = (double)(colorhex & 0xff) / 255;
@@ -75,9 +94,9 @@ t_rgb nearest_objs_color(t_object *nearest_obj)
     double red = nearest_obj->rgb->r;
     double green = nearest_obj->rgb->g;
     double blue = nearest_obj->rgb->b;
-    color = color_init2(red,green,blue);
-    // color = check_color_is_valid(color_init2(red,green,blue));
-    
+    // color = color_init2(red,green,blue);
+    color = check_color_is_valid(color_init2(red,green,blue));
+
     return color;
 }
 t_rgb get_light_color(t_rt *rt)
@@ -89,8 +108,8 @@ t_rgb get_light_color(t_rt *rt)
     double red = rt->scene->light->rgb->r;
     double green = rt->scene->light->rgb->g;
     double blue = rt->scene->light->rgb->b;
-    color = color_init2(red,green,blue);
-    // color = check_color_is_valid(color_init2(red,green,blue));
+    // color = color_init2(red,green,blue);
+    color = check_color_is_valid(color_init2(red,green,blue));
     return color;
 }
 t_rgb get_ambi_light_color(t_rt *rt)
@@ -102,8 +121,8 @@ t_rgb get_ambi_light_color(t_rt *rt)
     double red = rt->scene->ambi_light->rgb->r;
     double green = rt->scene->ambi_light->rgb->g;
     double blue = rt->scene->ambi_light->rgb->b;
-    color = color_init2(red,green,blue);
-    // color = check_color_is_valid(color_init2(red,green,blue));
+    // color = color_init2(red,green,blue);
+    color = check_color_is_valid(color_init2(red,green,blue));
     return color;
 }
 uint32_t color_ans(int r,int g , int b){
@@ -120,15 +139,12 @@ uint32_t color_ans(int r,int g , int b){
 uint32_t color2hex(t_rgb color)
 {
     printf("color.r:%f,color.g:%f,color.b:%f\n",color.r*255,color.g*255,color.b*255);
+    // printf("color.r:%f,color.g:%f,color.b:%f\n",color.r,color.g,color.b);
     // return (color_ans(color.r,color.g,color.b));
     return (color_ans(color.r*255,color.g*255,color.b*255));
 }
 int phong_calc(t_rt *rt, t_vec3 dir_vec, t_object *nearest_obj)
 {
-	// t_vec3  dsc = vec3_mul(*rt->scene->camera->nr_vec, (double)rt->width / (2 * tan((float)rt->scene->camera->view_degree / 2)));
-    // t_vec3 dir_vec = vec3_norm(vec3_add(screen_vec, dsc));
-    //TODO intersection計算があっているか見直す
-    // t_vec3 intersection = vec3_add(*rt->scene->camera->view_point, vec3_mul(dir_vec, calc_distance(nearest_obj, dir_vec, rt->scene->camera->view_point)));
      double t = calc_distance(nearest_obj, dir_vec, rt->scene->camera->view_point);
     t_vec3 intersection = vec3_add(*rt->scene->camera->view_point, vec3_mul(dir_vec, t));
     t_vec3 normal;
@@ -154,37 +170,27 @@ int phong_calc(t_rt *rt, t_vec3 dir_vec, t_object *nearest_obj)
     // printf("light_color.r:%f,light_color.g:%f,light_color.b:%f\n",light_color.r,light_color.g,light_color.b);
     amb =  color_mul(obj_color ,color_mul_scalar(get_ambi_light_color(rt),rt->scene->ambi_light->ratio));
     // double amb = rt->scene->ambi_light->ratio * rt->scene->light->factor->ka;
+    //拡散反射光
     double diff_deg = vec3_dot(normal, light_vec);
     // printf("diff_deg:%f\n",diff_deg);
     if(diff_deg < 0)
         diff_deg = 0;
     diff = color_mul_scalar(color_mul(obj_color,color_mul_scalar(light_color,rt->scene->light->bright_ratio)),diff_deg);
-    // t_rgb test = color_mul(obj_color,color_mul_scalar(light_color,rt->scene->light->bright_ratio));
-    // t_rgb test2 = color_mul_scalar(light_color,rt->scene->light->bright_ratio);
-    // printf("test.r:%f,test.g:%f,test.b:%f\n",test.r,test.g,test.b);
-    // printf("test2.r:%f,test2.g:%f,test2.b:%f\n",test2.r,test2.g,test2.b);
-    // printf("diff.r:%f,diff.g:%f,diff.b:%f\n",diff.r,diff.g,diff.b);
-    // printf("obj_color.r:%f,obj_color.g:%f,obj_color.b:%f\n",obj_color.r,obj_color.g,obj_color.b);
 
+
+    //鏡面反射
     t_vec3 v = vec3_mul(dir_vec, -1);
     t_vec3 r = vec3_sub(vec3_mul(vec3_mul(normal, vec3_dot(normal, light_vec)), 2), light_vec);
 	spec = color_mul_scalar(color_mul( rt->scene->light->factor->ks, color_mul_scalar(light_color,rt->scene->light->bright_ratio)), pow(vec3_dot(v, r), rt->scene->light->factor->shininess));
-	// spec = color_mul_scalar(color_mul( rt->scene->light->factor->ks, rt->scene->light->bright_ratio), pow(vec3_dot(v, r), rt->scene->light->factor->shininess));
-    // double spec = pow(vec3_dot(view_vec, vec3_reflect(vec3_mul(light_vec, -1), normal)), rt->scene->light->factor->shininess) * rt->scene->light->bright_ratio * rt->scene->light->factor->ks;
-    
+
     if(vec3_dot(v,r) < 0)
         spec = color_init2(0.0,0.0,0.0);
-    // printf("amb.r:%f,amb.g:%f,amb.b:%f\n",amb.r,amb.g,amb.b);
-    // printf("diff.r:%f,diff.g:%f,diff.b:%f\n",diff.r,diff.g,diff.b);
-    // printf("spec.r:%f,spec.g:%f,spec.b:%f\n",spec.r,spec.g,spec.b);
+
     if(is_shadow(rt->scene, nearest_obj, dir_vec))
     {
         diff = color_init2(0.0,0.0,0.0);
         spec = color_init2(0.0,0.0,0.0);
     }
-    // t_rgb amb_color = color_add(spec,diff);
-    // t_rgb color = color_add(amb,color_add(diff,spec));
-    // printf("color.r:%f,color.g:%f,color.b:%f\n",color.r,color.g,color.b);
-    // printf("amb_color.r:%f,amb_color.g:%f,amb_color.b:%f\n",amb_color.r,amb_color.g,amb_color.b);
+
     return color2hex(color_add(amb,color_add(diff,spec)));
 }
